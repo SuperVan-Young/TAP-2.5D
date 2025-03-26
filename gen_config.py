fa65b8eb8688ca871ff78899c7137df36a870d08
Chenhao Xue <xch927027@pku.edu.cn>
2025-03-26 10:59:09 +0800

    ablate fine-grained DVFS

--- gen_config.py
+++ gen_config.py
@@ -84,6 +84,14 @@ def main():
         config_path = os.path.join(TAP_25D_PATH, 'configs/test', f'{exp_name}.cfg')
         gen_quadgpu_config(config_path, output_path, freq_list)
 
+    # ablation study on fine-grained frequency setting
+    for i, boost in enumerate([(0,), (0, 1,), (0, 3,), (0, 1, 2,)]):
+        freq_list = [1.5 if c in boost else 0.9 for c in range(4)]
+        exp_name = 'quadgpu_separate-boost_%d' % i
+        output_path = os.path.join(TAP_25D_PATH, f'output/{exp_name}/')
+        config_path = os.path.join(TAP_25D_PATH, 'configs/test', f'{exp_name}.cfg')
+        gen_quadgpu_config(config_path, output_path, freq_list)
+
 # run all the experiments, and plot the thermal map
 def run_experiment(config_file):
     exp_name = config_file[:-4]
