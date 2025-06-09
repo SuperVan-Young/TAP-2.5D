fcc555c2ba2a22fa12590ce6bd484069740ca8b7
Chenhao Xue <xch927027@pku.edu.cn>
2025-06-09 16:32:59 +0800

    debug: - no convert and dump thermal map

--- config.py
+++ config.py
@@ -150,8 +150,8 @@ if __name__ == "__main__":
 	system.gen_flp(filename)
 	system.gen_ptrace(filename)
 	temp = system.run_hotspot(filename)
-	os.system('perl util/grid_thermal_map.pl ' + system.path+filename+'L4_ChipLayer.flp '+system.path + filename + '.grid.steady > '+system.path+filename+'.svg')
-	os.system('convert '+system.path + filename + '.svg '+system.path + filename + '.pdf')
+	# os.system('perl util/grid_thermal_map.pl ' + system.path+filename+'L4_ChipLayer.flp '+system.path + filename + '.grid.steady > '+system.path+filename+'.svg')
+	# os.system('convert '+system.path + filename + '.svg '+system.path + filename + '.pdf')
 	print ('temperature =', round(temp, 2), 'C,    takes', round(time.time()- start_time, 2), 'seconds')
 
 	# start_time = time.time()
